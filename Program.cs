fe0c0b33d7271a1df5de38043f559b2860d97429
Evans Bet <ebcheru@outlook.com>
2023-05-15 20:52:29 +0300

    My first C# code commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    class First
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to Coding");
+            Console.Write("Hello");
+            Console.Write("World");
+        }
+    }
+}
